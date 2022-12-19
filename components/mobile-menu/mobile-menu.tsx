abf341142618d9a5cc394e419317a990c4fcf785
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2022-12-19 23:58:40 +0200

    move mobile submenu into separate component

--- components/mobile-menu/mobile-menu.tsx
+++ components/mobile-menu/mobile-menu.tsx
@@ -4,6 +4,7 @@ import { Transition } from "@headlessui/react";
 import { Fragment, useEffect, useId, useState } from "react";
 import Link from "next/link";
 import Heading from "components/heading/heading";
+import MobileSubmenu from "./mobile-submenu";
 
 interface MobileMenuProps {
   items: any;
@@ -74,54 +75,7 @@ export default function MobileMenu({ items }: MobileMenuProps) {
                 )}
 
                 {item.sublinks && (
-                  <div>
-                    <button
-                      className="flex items-center justify-between w-full py-2 text-lg border-t border-gray-700"
-                      onClick={() => setSubMenuOpen(true)}
-                    >
-                      {item.title}
-                      <CgChevronRight className="mx-3 text-yellow-500 w-9 h-9" />
-                    </button>
-                    <Transition
-                      as={Fragment}
-                      show={subMenuOpen}
-                      enter="transition-all duration-[400ms]"
-                      enterFrom="ml-[700px]"
-                      enterTo="ml-0"
-                      leave="transition-all duration-[400ms]"
-                      leaveFrom="ml-0"
-                      leaveTo="ml-[700px]"
-                    >
-                      <div className="fixed top-0 left-0 z-10 w-screen h-screen bg-white dark:bg-gray-900 dark:text-white">
-                        <button
-                          className="p-3 mt-2"
-                          onClick={() => setSubMenuOpen(false)}
-                        >
-                          <div className="flex items-center gap-4">
-                            <CgChevronLeft className="w-10 h-10" />
-                            Main navigation
-                          </div>
-                        </button>
-                        <div className="px-4 py-10">
-                          <Heading level="h2" size="medium">
-                            {item.title}
-                          </Heading>
-                        </div>
-                        <div className="w-full h-screen px-4 overflow-scroll break-words">
-                          {item.sublinks.map((sublink, index) => (
-                            <div key={index}>
-                              <Link
-                                href={sublink.url}
-                                className="block py-3 text-lg border-t border-gray-700"
-                              >
-                                {sublink.title}
-                              </Link>
-                            </div>
-                          ))}
-                        </div>
-                      </div>
-                    </Transition>
-                  </div>
+                  <MobileSubmenu item={item.sublinks} title={item.title} />
                 )}
 
                 {/* Last item */}
