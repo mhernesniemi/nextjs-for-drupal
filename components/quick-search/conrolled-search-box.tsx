d2b39251734683237351a3a91faa759fe87359c8
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2022-12-25 18:30:42 +0200

    test auto focus keyboard on search

--- components/quick-search/conrolled-search-box.tsx
+++ components/quick-search/conrolled-search-box.tsx
@@ -6,6 +6,8 @@ import {
   RefObject,
   ComponentProps,
   Fragment,
+  useRef,
+  useCallback,
 } from "react";
 
 export type ControlledSearchBoxProps = ComponentProps<"div"> & {
@@ -28,41 +30,39 @@ export function ControlledSearchBox({
   value,
   ...props
 }: ControlledSearchBoxProps) {
-  function handleSubmit(event: FormEvent) {
-    event.preventDefault();
-    event.stopPropagation();
+  function focusAndOpenKeyboard(el) {
+    if (el) {
+      // Align temp input element approx. to be where the input element is
+      var __tempEl__ = document.createElement("input");
+      __tempEl__.style.position = "absolute";
+      __tempEl__.style.top = el.offsetTop + 7 + "px";
+      __tempEl__.style.left = el.offsetLeft + "px";
+      __tempEl__.style.height = "0";
+      __tempEl__.style.opacity = "0";
+      // Put this temp element as a child of the page <body> and focus on it
+      document.body.appendChild(__tempEl__);
+      __tempEl__.focus();
 
-    if (onSubmit) {
-      onSubmit(event);
-    }
-
-    if (inputRef.current) {
-      inputRef.current.blur();
+      // The keyboard is open. Now do a delayed focus on the target element
+      setTimeout(function () {
+        el.focus();
+        el.click();
+        // Remove the temp element
+        document.body.removeChild(__tempEl__);
+      }, 100);
     }
   }
 
-  function handleReset(event: FormEvent) {
-    event.preventDefault();
-    event.stopPropagation();
-
-    onReset(event);
-
-    if (inputRef.current) {
-      inputRef.current.focus();
-    }
-  }
+  const callbackRef = useCallback((inputElement: HTMLInputElement): void => {
+    callbackRef.current = inputElement;
+    focusAndOpenKeyboard(inputElement);
+  }, []);
 
   return (
     <Fragment {...props}>
-      <form
-        action=""
-        className="w-full"
-        noValidate
-        onSubmit={handleSubmit}
-        onReset={handleReset}
-      >
+      <form action="" className="w-full" noValidate>
         <input
-          ref={inputRef}
+          ref={callbackRef}
           className="w-full px-12 py-3 bg-gray-700 border border-gray-400 rounded placeholder:text-white"
           autoComplete="off"
           autoCorrect="off"
@@ -73,7 +73,6 @@ export function ControlledSearchBox({
           type="search"
           value={value}
           onChange={onChange}
-          autoFocus
         />
         <div className="absolute inset-y-0 flex items-center pl-3 pointer-events-none left-6">
           <GoSearch className="w-6 h-6 text-white" />
