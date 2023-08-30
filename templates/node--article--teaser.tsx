b5cf7e04b86c541c97e158c4a9df85fcd342f537
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2023-08-30 10:12:05 +0300

    add fields to article teaser

--- templates/node--article--teaser.tsx
+++ templates/node--article--teaser.tsx
@@ -14,7 +14,20 @@ export function NodeArticleTeaser({ node, ...props }: NodeArticleTeaserProps) {
 
   return (
     <div {...props}>
-      <h2 className="mb-4 text-3xl font-bold">Node id: {node.id}</h2>
+      <h2 className="mb-4 text-xl font-bold underline">
+        <Link href={node.path.alias ? node.path.alias : ""}>{node.title}</Link>
+      </h2>
+      <div className="mb-5">
+        <div>Date: {formatDate(node.created)}</div>
+        <div>User: {node.uid.display_name}</div>
+      </div>
+      <Image
+        src={absoluteUrl(node.field_media_image.field_media_image.uri.url)}
+        width={400}
+        height={200}
+        alt="Decorative image"
+        priority
+      />
     </div>
   );
 }
